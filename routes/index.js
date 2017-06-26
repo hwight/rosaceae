2bb4d751988b7e17c94d4ac790b3f8a237c90e09
Haley Wight <hwight@ramapo.edu>
2017-06-26 16:37:38 -0400

    firefox is weird

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Rosaceae Project' });
 });
 
+router.get('//', function(req, res, next) {
+  res.render('index', { title: 'The Rosaceae Project' });
+});
+
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'The Rosaceae Project' });
 });
